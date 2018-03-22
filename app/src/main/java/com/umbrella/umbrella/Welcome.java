183be716bb0b68437b1b2c78274d1f9afee7738e
hyunzeekim <hyunzeekim01@gmail.com>
2018-03-22 19:51:05 -0400

    welcome buttonclicks

--- app/src/main/java/com/umbrella/umbrella/Welcome.java
+++ app/src/main/java/com/umbrella/umbrella/Welcome.java
@@ -1,7 +1,9 @@
 package com.umbrella.umbrella;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Welcome extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void beginOnClick(View v){
+        startActivity(new Intent(Welcome.this, ChooseContacts.class));
+    }
+
+    public void settingOnClick(View v){
+        startActivity(new Intent(Welcome.this, SettingsPage.class));
     }
 }
