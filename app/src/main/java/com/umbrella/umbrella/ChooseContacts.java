20f6408ca6efda22016076ce0a5401a9151d812a
hyunzeekim <hyunzeekim01@gmail.com>
2018-03-23 22:25:58 -0400

    contacts

--- app/src/main/java/com/umbrella/umbrella/ChooseContacts.java
+++ app/src/main/java/com/umbrella/umbrella/ChooseContacts.java
@@ -1,7 +1,9 @@
 package com.umbrella.umbrella;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseContacts extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ChooseContacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_contacts);
+    }
+
+    public void shapeOnClick(View v){
+        startActivity(new Intent(ChooseContacts.this, ShapePage.class));
     }
 }
