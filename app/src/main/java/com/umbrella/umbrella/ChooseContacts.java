f328246d103b3d17ecb6888f701539e89c46511f
cynnnzhang <cynthiazheart@hotmail.com>
2018-03-25 23:08:47 -0400

    Spinner
    Spinner for Time Interval drop down options

--- app/src/main/java/com/umbrella/umbrella/ChooseContacts.java
+++ app/src/main/java/com/umbrella/umbrella/ChooseContacts.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class ChooseContacts extends AppCompatActivity {
 
@@ -13,6 +16,13 @@ public class ChooseContacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_contacts);
+
+        Spinner timeInterval = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(ChooseContacts.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.intervals));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        timeInterval.setAdapter(myAdapter);
     }
 
     public void shapeOnClick(View v){
