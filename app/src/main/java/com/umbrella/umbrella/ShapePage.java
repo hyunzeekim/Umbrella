51c7b3b40c6b41b00741f225970cac70f811a216
cynnnzhang <cynthiazheart@hotmail.com>
2018-04-08 13:38:34 -0400

    Countdown 2

--- app/src/main/java/com/umbrella/umbrella/ShapePage.java
+++ app/src/main/java/com/umbrella/umbrella/ShapePage.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.os.CountDownTimer;
+import android.content.Intent;
 
 public class ShapePage extends AppCompatActivity {
 
@@ -12,9 +13,23 @@ public class ShapePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shape_page);
-        countdown = (TextView) findViewById(R.id.text_view_countdown);
-        new CountDownTimer(30000, 1000) {
 
+        countdown = (TextView) findViewById(R.id.text_view_countdown);
+
+        Intent timeSpinner = getIntent();
+        String result = timeSpinner.getStringExtra("timeInterval");
+        int value = 0;
+        if (result.equalsIgnoreCase("5 Minutes")){
+            value = 5*60*1000;
+        }
+        if (result.equalsIgnoreCase("10 Minutes")){
+            value = 10*60*1000;
+        }
+        else if (result.equalsIgnoreCase("15 Minutes")){
+            value = 15*60*1000;
+        }
+
+            new CountDownTimer(30000, 1000) {
             public void onTick(long millisUntilFinished) {
                 countdown.setText("seconds remaining: " + millisUntilFinished / 1000);
             }
