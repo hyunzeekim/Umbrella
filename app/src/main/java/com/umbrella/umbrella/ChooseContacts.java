88ea509b7a642265af9f7689b4de7a97494592ac
cynnnzhang <cynthiazheart@hotmail.com>
2018-04-15 02:33:40 -0400

    Button
    Contact Image Button, it clicks, but doesn't currently do anything

--- app/src/main/java/com/umbrella/umbrella/ChooseContacts.java
+++ app/src/main/java/com/umbrella/umbrella/ChooseContacts.java
@@ -3,19 +3,22 @@ package com.umbrella.umbrella;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ChooseContacts extends AppCompatActivity {
 
-    private EditText Contact1;
-
     String number;
     Spinner timeInterval;
+    ImageButton b1;
+    ImageButton b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +32,33 @@ public class ChooseContacts extends AppCompatActivity {
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         timeInterval.setAdapter(myAdapter);
 
-        Contact1 = (EditText)findViewById(R.id.etContact1);
+        b1 = (ImageButton) findViewById(R.id.b1);
+        b2 =(ImageButton)findViewById(R.id.b2);
 
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Contact 1 was chosen", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Contact 2 was chosen", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
     }
 
     public void shapeOnClick(View v){
         Intent timeInterval = new Intent(ChooseContacts.this, ShapePage.class);
         timeInterval.putExtra("timeInterval", this.timeInterval.getSelectedItem().toString());
         startActivity(timeInterval);
+
+    }
+
+    public void b1OnClick(View v){
 
     }
 
