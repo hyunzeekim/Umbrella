4fd4dd6801eb5a2e5457a289f5e4a8448147b44e
mirandawoohoo <mirandawu12208@gmail.com>
2018-04-15 01:42:06 -0400

    shape stuff

---
+++ app/src/main/java/com/umbrella/umbrella/CheckIn.java
@@ -0,0 +1,35 @@
+package com.umbrella.umbrella;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+                String password = preferences.getString("password", "0");
+                if(password.equals("0")){
+                    Intent intent = new Intent(getApplicationContext(), CreatePasswordActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), InputPasswordActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+        }, 2000);
+    }
+}
